a6f80fa74da4c44add59b2f21d6bbcbbf02c40cb
bogdanGR <fant1k450@gmail.com>
2024-05-26 12:04:19 +0300

    Add types and statuses to project model and static getters

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -10,4 +10,27 @@ class Project extends Model
     use HasFactory;
 
     public $timestamps = true;
+
+    const TYPE_EU = 1;
+    const TYPE_GR = 2;
+
+    const STATUS_IN_PROGRESS = 1;
+    const STATUS_COMPLETED = 2;
+
+
+    public static function getTypes()
+    {
+        return [
+          self::TYPE_EU => 'EU Funding',
+          self::TYPE_GR => 'Greek Funding',
+        ];
+    }
+
+    public static function getStatuses()
+    {
+        return [
+            self::STATUS_IN_PROGRESS => 'In Progress',
+            self::STATUS_COMPLETED => 'Completed',
+        ];
+    }
 }
